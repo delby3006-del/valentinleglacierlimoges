7d55b224c06227d8c52e56953c7252f137465dc8
delby3006-del <delby3006@gmail.com>
2026-05-12 16:49:19 +0200

    Mise a jour responsive et configuration production

--- backend/db.js
+++ backend/db.js
@@ -1,22 +1,3 @@
-// const { Pool } = require("pg");
-
-// const pool = new Pool(
-//   process.env.DATABASE_URL
-//     ? {
-//         connectionString: process.env.DATABASE_URL,
-//         ssl: { rejectUnauthorized: false },
-//       }
-//     : {
-//         user: "postgres",
-//         host: "172.xxx.xxx.xxx",
-//         database: "valentin_glacier",
-//         password: "gogo12",
-//         port: 5432,
-//       },
-// );
-
-// module.exports = pool;
-
 const { Pool } = require("pg");
 require("dotenv").config();
 
